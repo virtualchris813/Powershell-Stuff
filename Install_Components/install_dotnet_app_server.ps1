0af6347d1a1166789ce268b39c63b1c201fadeb0
Chris Adams <virtualchris813@gmail.com>
2019-10-07 13:12:51 -0600

    Adding command for install Windows components

---
+++ Install_Components/install_dotnet_app_server.ps1
@@ -0,0 +1,3 @@
+Install-WindowsFeature -Name Web-Windows-Auth,Web-ISAPI-Filter,Web-ISAPI-Ext,Web-Net-Ext45,NET-Framework-45-ASPNET,NET-Framework-45-Core -IncludeManagementTools -Restart
+
+# Get-WindowsFeature will provide a list of the features available
